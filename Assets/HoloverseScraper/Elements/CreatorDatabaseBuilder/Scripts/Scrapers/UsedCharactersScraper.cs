498e4211c4b76362eeab61b13d0305b9051a18fe
Admor Aguilar <admor.aguilar@gmail.com>
2020-10-20 01:26:23 +0800

    * Update UsedCharacterScrapers to use assigned text assets. * Generate new UsedCharactersFile.txt

--- Assets/HoloverseScraper/Elements/CreatorDatabaseBuilder/Scripts/Scrapers/UsedCharactersScraper.cs
+++ Assets/HoloverseScraper/Elements/CreatorDatabaseBuilder/Scripts/Scrapers/UsedCharactersScraper.cs
@@ -8,26 +8,37 @@ namespace Holoverse.Scraper
 {
 	public class UsedCharactersScraper : MonoBehaviour
 	{
+		[SerializeField]
+		private TextAsset[] _textAssets = null;
+
 		private HashSet<char> _usedCharactersSet = new HashSet<char>();
 
 		[ContextMenu("Run")]
 		public void Run()
 		{
 			Debug.Log($"Start used characters scraper.");
-			string folderPath = PathUtilities.CreateDataPath("HoloverseScraper", string.Empty, PathType.Data);
-			List<string> ext = new List<string>() { "json" };
-			IEnumerable<string> filePaths = Directory
-				.EnumerateFiles(folderPath, "*", SearchOption.AllDirectories)
-				.Where(f => ext.Contains(Path.GetExtension(f).TrimStart('.').ToLowerInvariant()));
-			foreach(string filePath in filePaths) {
-				string text = File.ReadAllText(filePath);
-				foreach(char ch in text) { _usedCharactersSet.Add(ch); }
+
+			if(_textAssets != null || _textAssets.Length > 0) {
+				foreach(TextAsset textAsset in _textAssets) {
+					foreach(char ch in textAsset.text) { _usedCharactersSet.Add(ch); }
+				}
+			} else {
+				string folderPath = PathUtilities.CreateDataPath("HoloverseScraper", string.Empty, PathType.Data);
+				List<string> ext = new List<string>() { "json" };
+				IEnumerable<string> filePaths = Directory
+					.EnumerateFiles(folderPath, "*", SearchOption.AllDirectories)
+					.Where(f => ext.Contains(Path.GetExtension(f).TrimStart('.').ToLowerInvariant()));
+				foreach(string filePath in filePaths) {
+					string text = File.ReadAllText(filePath);
+					foreach(char ch in text) { _usedCharactersSet.Add(ch); }
+				}
 			}
 
 			Debug.Log($"Finished. Total characters used: {_usedCharactersSet.Count}");
 
 			string allCharacters = new string(_usedCharactersSet.ToArray());
-			File.WriteAllText(PathUtilities.CreateDataPath("HoloverseScraper", "usedCharacters.txt", PathType.Data), allCharacters);
+			string outputFilePath = PathUtilities.CreateDataPath("HoloverseScraper", "usedCharacters.txt", PathType.Data);
+			File.WriteAllText(AssetUtilities.CreatePath(outputFilePath), allCharacters);
 		}
 	}
 }
