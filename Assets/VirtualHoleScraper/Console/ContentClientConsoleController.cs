4c53841325303b13085cfee5f2a5d09e242ab237
Admor Aguilar <admor.aguilar@gmail.com>
2020-11-14 23:59:30 +0800

    * Test Server build

---
+++ Assets/VirtualHoleScraper/Console/ContentClientConsoleController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using Midnight;
+
+namespace VirtualHole.Scraper
+{
+	public class ContentClientConsoleController : MonoBehaviour
+	{
+		private void Start()
+		{
+			MLog.Log("Hello world from Unity console.");
+		}
+	}
+}
